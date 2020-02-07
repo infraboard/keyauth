0630b99a6d2d41e6b2fcb30316c6a2524af6fcef
yumaojun03 <18108053819@163.com>
2020-02-07 10:42:06 +0800

    定义角色

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -2,13 +2,20 @@ package project
 
 // Project tenant resource container
 type Project struct {
-	ID          string `json:"id,omitempty"`          // 项目唯一ID
+	ID string `json:"id,omitempty"` // 项目唯一ID
+	*CreateProjectRequest
+	DomainID string `json:"domain_id,omitempty"` // 所属域ID
+	CreateAt int64  `json:"create_at,omitempty"` // 创建时间
+	UpdateAt int64  `json:"update_at,omitempty"` // 项目修改时间
+}
+
+// CreateProjectRequest 创建项目请求
+type CreateProjectRequest struct {
 	Name        string `json:"name,omitempty"`        // 项目名称
 	Picture     string `json:"picture,omitempty"`     // 项目描述图片
+	Type        string `json:"type,omitempty"`        // 项目类型
 	Enabled     bool   `json:"enabled,omitempty"`     // 禁用项目, 该项目所有人暂时都无法访问
-	Owner       string `json:"owner_id,omitempty"`    // 项目所有者, 一般为创建者
+	Owner       string `json:"owner_id,omitempty"`    // 项目所有者, PMO
+	Region      string `json:"region,omitempty"`      // 项目所属地区
 	Description string `json:"description,omitempty"` // 项目描述
-	DomainID    string `json:"domain_id,omitempty"`   // 所属域ID
-	CreateAt    int64  `json:"create_at,omitempty"`   // 创建时间
-	UpdateAt    int64  `json:"update_at,omitempty"`   // 项目修改时间
 }
--- pkg/role/role.go
+++ pkg/role/role.go
@@ -27,14 +27,43 @@ tk
 
 package role
 
+// Type 角色类型
+type Type string
+
+const (
+	// BuildInType 内建角色, 系统初始时创建
+	BuildInType Type = "build_in"
+	// CustomType 用户自定义角色
+	CustomType Type = "custom"
+)
+
+// EffectType 授权效力包括两种：允许（Allow）和拒绝（Deny）
+type EffectType string
+
+const (
+	// Allow 允许访问
+	Allow EffectType = "allow"
+	// Deny 拒绝访问
+	Deny EffectType = "deny"
+)
+
 // Role is rbac's role
 type Role struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`                  // 角色名称
-	Description string `json:"description,omitempty"` // 角色描述
-	CreateAt    int64  `json:"create_at,omitempty"`   // 创建时间`
-	UpdateAt    int64  `json:"update_at,omitempty"`   // 更新时间
+	Name        string      `json:"name"`                  // 角色名称
+	Description string      `json:"description,omitempty"` // 角色描述
+	CreateAt    int64       `json:"create_at,omitempty"`   // 创建时间`
+	UpdateAt    int64       `json:"update_at,omitempty"`   // 更新时间
+	Read        *Permission `json:"read,omitempty"`        // 读权限
+	Write       *Permission `json:"write,omitempty"`       // 写权限
+	Type        Type        `json:"type"`
+}
 
+// Permission 权限
+type Permission struct {
+	Effect   EffectType `json:"effect,omitempty"`
+	IsALL    bool       `json:"is_all"`
+	Resource []string   `json:"resource,omitempty"` // 资源列表
+	Scope    []string   `json:"scope,omitempty"`    // 范围列表
 }
 
 // CheckPermission 检测该角色是否具有该权限
