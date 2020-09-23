b8129342c1da8604d273149970e2886bd2025938
jamesduan <jamesduan@tencent.com>
2020-09-23 15:41:45 +0800

    fix loadConfigFunc

--- conf/load.go
+++ conf/load.go
@@ -26,7 +26,7 @@ func LoadConfigFromToml(filePath string) error {
 	if _, err := toml.DecodeFile(filePath, cfg); err != nil {
 		return err
 	}
-
+	global = cfg
 	return cfg.InitGloabl()
 }
 
@@ -36,6 +36,6 @@ func LoadConfigFromEnv() error {
 	if err := env.Parse(cfg); err != nil {
 		return fmt.Errorf("load config from env, %s", err.Error())
 	}
-
+	global = cfg
 	return cfg.InitGloabl()
 }
