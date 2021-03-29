fa3a7a3041760dd557e6b4e5f6986359b147a59b
yumaojun03 <18108053819@163.com>
2021-03-29 23:20:41 +0800

    补充scope

--- client/auth_interceptor.go
+++ client/auth_interceptor.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/infraboard/mcube/exception"
 	"github.com/infraboard/mcube/grpc/gcontext"
+	"github.com/infraboard/mcube/http/request"
 	"github.com/infraboard/mcube/logger"
 	"github.com/infraboard/mcube/logger/zap"
 	httpb "github.com/infraboard/mcube/pb/http"
@@ -18,6 +19,7 @@ import (
 	"github.com/infraboard/keyauth/pkg/endpoint"
 	"github.com/infraboard/keyauth/pkg/micro"
 	"github.com/infraboard/keyauth/pkg/permission"
+	"github.com/infraboard/keyauth/pkg/policy"
 	"github.com/infraboard/keyauth/pkg/token"
 	"github.com/infraboard/keyauth/pkg/user/types"
 )
@@ -169,10 +171,20 @@ func (a *GrpcAuther) validatePermission(ctx *gcontext.GrpcInCtx, path string) (*
 		req := permission.NewCheckPermissionRequest()
 		req.EndpointId = eid
 		req.NamespaceId = ctx.GetNamespace()
-		_, err = a.c.Permission().CheckPermission(outCtx.Context(), req)
+		perm, err := a.c.Permission().CheckPermission(outCtx.Context(), req)
 		if err != nil {
 			return nil, exception.NewPermissionDeny("no permission, %s", err)
 		}
+
+		queryP := policy.NewQueryPolicyRequest(request.NewPageRequest(500, 1))
+		queryP.Account = tk.Account
+		queryP.NamespaceId = ctx.GetNamespace()
+		queryP.RoleId = perm.RoleId
+		ps, err := a.c.Policy().QueryPolicy(outCtx.Context(), queryP)
+		if err != nil {
+			return nil, err
+		}
+		tk.Scope = ps.GetScope(tk.Account)
 	}
 
 	return tk, nil
