8ade465c74c6554a36db2e3d36f4ee389542201b
yumaojun03 <18108053819@163.com>
2021-04-28 10:34:18 +0800

    修复 admin创建问题

--- pkg/user/mongo/user.go
+++ pkg/user/mongo/user.go
@@ -43,6 +43,7 @@ func (s *service) CreateAccount(ctx context.Context, req *user.CreateAccountRequ
 	}
 
 	u, err := user.New(req)
+	u.Type = types.UserType(user.CreateType_DOMAIN_CREATED)
 	if err != nil {
 		return nil, err
 	}
